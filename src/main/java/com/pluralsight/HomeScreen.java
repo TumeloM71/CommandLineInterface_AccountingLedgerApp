1f9e5e935efe99b2af0829f5f17d945e7cb4fc92
Tumelo <tumelomarongwe@gmail.com>
2024-10-15 15:27:58 -0500

    Program should meet all the given objectives now. Should only need to add the README.md file now

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Scanner;
--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Reports {
     static Scanner input;
@@ -16,6 +16,7 @@ public class Reports {
             System.out.println("3) Year to Date");
             System.out.println("4) Previous Year");
             System.out.println("5) Search by Vendor");
+            System.out.println("6) Custom Search");
             System.out.println("0) Back");
 
             userInput =  Utilities.getIntValue(input.nextLine().trim());
@@ -36,6 +37,8 @@ public class Reports {
                 case 5:
                     searchByVendor(transactions);
                     break;
+                case 6:
+                    customSearch(transactions);
                 case 0:
                     return;
             }
@@ -72,7 +75,7 @@ public class Reports {
         LocalDate prev = now.minusYears(1);
 
         for( Transaction t : transactions){
-            if( t.getDate().isAfter(prev) && t.getDate().isBefore(now))
+            if( t.getDate().compareTo(prev)>=0 && t.getDate().compareTo(now)<=0)
                 System.out.println(t);
         }
     }
@@ -82,9 +85,65 @@ public class Reports {
         LocalDate prev = now.minusMonths(1);
 
         for( Transaction t : transactions){
-            if( t.getDate().isAfter(prev) && t.getDate().isBefore(now))
+            if( t.getDate().compareTo(prev)>=0 && t.getDate().compareTo(now)<=0)
                 System.out.println(t);
         }
+    }
+
+    public static void customSearch(ArrayList<Transaction> transactions){
+        ArrayList<Transaction> filteredList = new ArrayList<>(transactions); //Stored filtered elements
+        ArrayList<Transaction> filter = new ArrayList<>(); //Stores elements removed by filters
+
+        System.out.print("Start Date:");
+        String startDate = input.nextLine().trim();
+        System.out.print("End date: ");
+        String endDate = input.nextLine().trim();
+        System.out.print("Description: ");
+        String description = input.nextLine().trim();
+        System.out.print("Vendor: ");
+        String vendor = input.nextLine().trim();
+        System.out.println("Amount" );
+        String amount = input.nextLine().trim();
+
+        //If the user enters a field, add all elements which don't match to the filter list
+        if (!startDate.isEmpty()){
+            for(Transaction t : transactions){
+                if ( t.getDate().compareTo(Utilities.getDate(startDate)) < 0)
+                    filter.add(t);
+            }
+        }
+        if (!endDate.isEmpty()){
+            for(Transaction t : transactions){
+                if( t.getDate().compareTo(Utilities.getDate(endDate))>0)
+                    filter.add(t);
+            }
+        }
+
+        if(!description.isEmpty()){
+            for( Transaction t : transactions){
+                if( !t.getDescription().equalsIgnoreCase(description))
+                    filter.add(t);
+            }
+        }
+
+        if(!vendor.isEmpty()){
+            for( Transaction t : transactions){
+                if( !t.getVendor().equalsIgnoreCase(vendor))
+                    filter.add(t);
+            }
+        }
+
+        if(!amount.isEmpty()){
+            for( Transaction t : transactions){
+                if( !(t.getAmount() == Utilities.getDoubleValue(amount)) )
+                    filter.add(t);
+            }
+        }
+        //Remove all elements whcih don't the given filters by calling removeAll()
+        filteredList.removeAll(filter);
+        for( Transaction t : filteredList)
+            System.out.println(t);
+
     }
 
 }
